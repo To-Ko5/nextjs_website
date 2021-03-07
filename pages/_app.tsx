f2c2ebf7bb28ab34f988c424dfa92f2b0a949a84
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-08 08:19:07 +0900

    Fix: 型を追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import 'tailwindcss/tailwind.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
