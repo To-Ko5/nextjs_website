8f2c626f9714384a234fff791de8d4fdd5cbd524
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-09 08:00:23 +0900

    Add: Layout components

---
+++ components/Layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+import Link from 'next/link'
+
+interface layout {
+  children: React.ReactNode
+  title?: string
+}
+
+const Layout = ({ children, title = 'title' }: layout) => {
+  return (
+    <div className="min-h-screen py-0 px-2 flex flex-col justify-center items-center text-gray-600 text-sm font-mono">
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main className="flex flex-1 justify-center items-center flex-col w-screen">
+        {children}
+      </main>
+    </div>
+  )
+}
+
+export default Layout
