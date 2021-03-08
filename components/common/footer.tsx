6852e6c34f3788e284e4821c843e5286b1ad16ae
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-09 08:19:55 +0900

    Add: footer components

---
+++ components/common/footer.tsx
@@ -0,0 +1,17 @@
+const footer = () => {
+  return (
+    <footer className="w-full h-12 flex justify-center items-center border-t">
+      <a
+        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex items-center"
+      >
+        Powered by
+        <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+      </a>
+    </footer>
+  )
+}
+
+export default footer
