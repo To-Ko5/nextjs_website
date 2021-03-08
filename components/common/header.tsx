33dfe0f19a0ce7023de4c33d0e5f0577d0a1884a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-09 08:19:32 +0900

    Fix: styleを修正

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,31 +1,29 @@
 import Link from 'next/link'
 const header = () => {
   return (
-    <div>
-      <header>
-        <nav className="bg-gray-800 w-screen">
-          <div className="flex items-center pl-8 h-14">
-            <div className="flex space-x-4">
-              <Link href="/">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
-                  Home
-                </a>
-              </Link>
-              <Link href="/blog">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
-                  Blog
-                </a>
-              </Link>
-              <Link href="/contact">
-                <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
-                  Contact
-                </a>
-              </Link>
-            </div>
+    <header className="bg-gray-800 w-screen">
+      <nav>
+        <div className="flex items-center pl-8 h-14">
+          <div className="flex space-x-4">
+            <Link href="/">
+              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                Home
+              </a>
+            </Link>
+            <Link href="/blog">
+              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                Blog
+              </a>
+            </Link>
+            <Link href="/contact">
+              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+                Contact
+              </a>
+            </Link>
           </div>
-        </nav>
-      </header>
-    </div>
+        </div>
+      </nav>
+    </header>
   )
 }
 
