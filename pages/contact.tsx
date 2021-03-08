c2f4839bd9ae8a0dd56058fea689fcdff6359bc4
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-09 08:13:08 +0900

    Add: layout componentsを読み込む

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,5 +1,11 @@
+import Layout from '../components/Layout'
+
 function Contact() {
-  return <div></div>
+  return (
+    <Layout title="Blog">
+      <h1 className="text-4xl">Blog</h1>
+    </Layout>
+  )
 }
 
 export default Contact
