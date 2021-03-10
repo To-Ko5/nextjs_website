f792291d37eb3c7739f0e50221eb0241d79f9d00
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-11 08:40:37 +0900

    Add: contents add

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,24 @@ import Image from 'next/image'
 function Contact() {
   return (
     <Layout title="Blog">
-      <h1 className="text-4xl">Blog</h1>
+      <div className="bg-white text-center shadow-xl p-8 w-80 rounded">
+        <div className="mt-4">
+          <h1 className="font-bold">Blog</h1>
+        </div>
+        <div className="flex justify-center mt-4">
+          <Image
+            className="rounded-full"
+            src="/icon.png"
+            width={80}
+            height={80}
+            alt="icon画像"
+          />
+        </div>
+        <div className="mt-4">
+          <p className="font-bold">E-Mail</p>
+          <p className="text-s mt-2 text-gray-700">demo@gmail.com</p>
+        </div>
+      </div>
     </Layout>
   )
 }
