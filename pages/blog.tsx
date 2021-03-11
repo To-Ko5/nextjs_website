d576976f886e138a0df4849f73a9aac1cf290894
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-11 10:13:04 +0900

    Add: データの取得を追加

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,11 +1,23 @@
 import Layout from '../components/Layout'
+import Post from '../components/blogs/Post'
 
-const Blog = () => {
+import { getAllPostData } from '../Library/posts'
+
+const Blog = ({ posts }) => {
   return (
     <Layout title="Blog">
-      <h1 className="text-4xl">Blog</h1>
+      <div className="m-10 text-left">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </div>
     </Layout>
   )
 }
 
 export default Blog
+
+export async function getStaticProps() {
+  const posts = await getAllPostData()
+  return {
+    props: { posts }
+  }
+}
