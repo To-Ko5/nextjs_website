d10f1c6d6db72292430be407cf05acbf5d00423d
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-11 10:12:28 +0900

    first commit

---
+++ components/blogs/Post.tsx
@@ -0,0 +1,12 @@
+const Post = ({ post }) => {
+  return (
+    <div className="mb-2">
+      <span>{post.id}</span>:
+      <span className="cursor-pointer text-blue-400 border-b brder-blue-400 hover:bg-gray-300">
+        {post.title}
+      </span>
+    </div>
+  )
+}
+
+export default Post
