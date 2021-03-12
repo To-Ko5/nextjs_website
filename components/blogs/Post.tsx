efe4bd35334fa75f675279381d3635d4f9745b1e
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-12 09:23:27 +0900

    Add: Linkを追加

--- components/blogs/Post.tsx
+++ components/blogs/Post.tsx
@@ -1,10 +1,14 @@
+import Link from 'next/link'
+
 const Post = ({ post }: any) => {
   return (
     <div className="mb-2">
       <span>{post.id}</span>:
-      <span className="cursor-pointer text-blue-400 border-b brder-blue-400 hover:bg-gray-300">
-        {post.title}
-      </span>
+      <Link href={`posts/${post.id}`}>
+        <span className="cursor-pointer text-blue-400 border-b brder-blue-400 hover:bg-gray-300">
+          {post.title}
+        </span>
+      </Link>
     </div>
   )
 }
