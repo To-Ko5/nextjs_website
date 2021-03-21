dcec5656431986d9cb35d95ce164ef366d28380a
To-Ko5 <kobayashi.toshi.tk@gmail.com>
2021-03-21 10:26:09 +0900

    Add: data-testidを追加

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -6,17 +6,26 @@ const header = () => {
         <div className="flex items-center pl-8 h-14">
           <div className="flex space-x-4">
             <Link href="/">
-              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+              <a
+                data-testid="home-nav"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+              >
                 Home
               </a>
             </Link>
             <Link href="/blog">
-              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+              <a
+                data-testid="blog-nav"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+              >
                 Blog
               </a>
             </Link>
             <Link href="/contact">
-              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+              <a
+                data-testid="contact-nav"
+                className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded"
+              >
                 Contact
               </a>
             </Link>
